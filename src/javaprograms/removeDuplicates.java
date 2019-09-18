e20eac9184174d23ba10b52a7e29d49958c19de3
rakhiawari <rakhiawari98@gmail.com>
2019-09-18 20:19:09 +0530

    Add all files

--- src/javaprograms/removeDuplicates.java
+++ src/javaprograms/removeDuplicates.java
@@ -1,12 +1,23 @@
 package javaprograms;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class removeDuplicates {
 
-    
+
+    public String removeDuplicate(String str) {
+        Set<Character> newString = new LinkedHashSet<>();
+        for (int i = 0; i <= str.length() - 1; i++) {
+            newString.add(str.charAt(i));
+        }
+        return newString.toString();
+    }
     public static void main(String[] args) {
 
         Scanner scanner=new Scanner(System.in);
+        String str1=scanner.nextLine();
+        System.out.println(new removeDuplicates().removeDuplicate(str1));
     }
 }
